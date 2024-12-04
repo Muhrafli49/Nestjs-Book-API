2fac750bfb290ae16917a22fb0e31f3b1c41370e
Rafli <mraflie49@gmail.com>
2024-12-04 23:00:35 +0700

    feat: create auth entity for database schema

---
+++ src/auth/entity/refresh-token.entity.ts
@@ -0,0 +1,17 @@
+import { BaseEntity, Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from 'src/users/entity/user.entity';
+
+@Entity()
+export class RefreshToken extends BaseEntity{
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    isRevoked: boolean;
+
+    @Column()
+    expiredAt: Date;
+
+    @ManyToOne(() => User, (user) => user.refreshTokens)
+    user: User;
+}
