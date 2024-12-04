c517934b3d269853976887e74b6b7dbe94a27f66
Rafli <mraflie49@gmail.com>
2024-12-04 22:55:16 +0700

    feat: add filter-book DTO for book filtering

---
+++ src/books/dto/filter-book.dto.ts
@@ -0,0 +1,25 @@
+import { IsOptional, IsInt } from "class-validator";
+import { Type } from "class-transformer";
+
+export class FilterBookDto {
+    @IsOptional()
+    title: string;
+
+    @IsOptional()
+    author: string;
+
+    @IsOptional()
+    category: string;
+
+    @IsOptional()
+    @IsInt()
+    @Type(() => Number)
+    min_year: number;
+
+    @IsOptional()
+    @IsInt()
+    @Type(() => Number)
+    max_year: number;
+
+
+}
